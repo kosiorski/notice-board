64e202ac8aaaf597126490fd007e11456534554c
k0sa <kosiorski.tomasz@gmail.com>
2019-01-05 10:42:53 +0100

    first commit

---
+++ src/main/java/pl/kosiorski/NoticeBoardApplication.java
@@ -0,0 +1,14 @@
+package pl.kosiorski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeBoardApplication.class, args);
+    }
+
+}
+
